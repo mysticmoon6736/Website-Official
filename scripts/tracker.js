d1168cb9e374d41e3a5e42a7a30a9349562784f2
mysticmoon6736 <chambersmadisonisaiah@gmail.com>
2025-02-08 23:43:12 -0500

    Update tracker.js

--- scripts/tracker.js
+++ scripts/tracker.js
@@ -1,5 +1,1 @@
-function showSection(section) {
-    document.getElementById("sims").style.display = "none";
-    document.getElementById("minecraft").style.display = "none";
-    document.getElementById(section).style.display = "block";
-}
+
